7164d05e37166b4ddf26fe7d18535b7d819f8c40
sonynanaVN <samsamsam0905@gmail.com>
2025-03-10 20:16:22 +0700

    Update while study git

---
+++ bai2-6.php
@@ -0,0 +1,27 @@
+
+<?php
+//phuong thuc class oop-ke thua
+// lop ke thua giup chung ta tao mot lop moi va
+// thua huong cac thuoc tinh mot lop da ton tai trước đó
+// giúp kế thừa tài nguyên đã tồn tại và dùng lại
+// Chỉ kế thừa được phương thức public và protected
+class A{
+    //private public $attr_1:Neu khai bao pt private,chi hoat dong trong class A
+    public $attr_1;
+    // ......
+    function method_1(){
+       //.....
+    echo "ok"; 
+    }
+}
+class B extends A{
+    public $attr_2;
+    public function method_2(){
+        echo $this->attr_2;
+    }
+}
+//Khai bao phuong thuc moi
+$b= new B;
+// Trỏ vào 
+$b -> attr_2 ="Class b";
+echo $b->method_1();
