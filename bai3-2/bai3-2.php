857ce3fdbf08a626e49dffa03976b13fc6e39940
sonynanaVN <samsamsam0905@gmail.com>
2025-03-11 21:07:22 +0700

    Update while study git

--- bai3-2/bai3-2.php
+++ bai3-2/bai3-2.php
@@ -2,12 +2,12 @@
 //tao database phpmyadmin va cau hinh ket noi co so du lieu
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','localhost');
+define('DB_PASS','');
 define('DB_NAME','lavabelpro ');
 
 $db = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-if($db->connect_errol){
-    die("kết nối cơ sở dữ liệu k thành công") .$db->connect_errol;
+if($db->connect_error){
+    die("kết nối cơ sở dữ liệu k thành công".$db->connect_error);
 
 }
 echo "Kết nối thành công";
