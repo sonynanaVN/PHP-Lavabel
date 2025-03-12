68e0081533db5999e3ac8ac5dac51b38221d11b7
sonynanaVN <samsamsam0905@gmail.com>
2025-03-12 21:24:23 +0700

    Update while study git

--- bai3-3/bai3-3.php
+++ bai3-3/bai3-3.php
@@ -5,9 +5,9 @@ class DB{
     function connection(){
         $this->conn=new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
         if ($this->conn->connect_error){
-            die("Kết nối dữ liệu k thành công".$this->connect_error);
+            die("Kết nối dữ liệu k thành công".$this->conn->connect_error);
         }echo "Đã kết nối thành công";
     }
-    $db =new DB;
-    $db -> connection();
 }
+ $db =new DB;
+    $db -> connection();
