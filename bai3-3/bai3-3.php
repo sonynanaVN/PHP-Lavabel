a963400d594c312e07f99ccaa1a2a9c1f3ceb866
sonynanaVN <samsamsam0905@gmail.com>
2025-03-12 21:23:52 +0700

    Update while study git

---
+++ bai3-3/bai3-3.php
@@ -0,0 +1,13 @@
+<?php
+require "config.php";
+class DB{
+    public $conn;
+    function connection(){
+        $this->conn=new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+        if ($this->conn->connect_error){
+            die("Kết nối dữ liệu k thành công".$this->connect_error);
+        }echo "Đã kết nối thành công";
+    }
+    $db =new DB;
+    $db -> connection();
+}
